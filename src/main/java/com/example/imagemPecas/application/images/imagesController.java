b5aa24838b1d2a69652af48d38386e17afa5268b
manuhcr <mitskiws@gmail.com>
2026-09-10 19:57:24 -0300

    continuação

--- src/main/java/com/example/imagemPecas/application/images/imagesController.java
+++ src/main/java/com/example/imagemPecas/application/images/imagesController.java
@@ -5,6 +5,7 @@ import com.example.imagemPecas.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,8 +50,13 @@ public class imagesController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(image.getExtension().getMediaType());
         headers.setContentLength(image.getSize());
-        headers.setContentDispositionFormData("", image.getName().concat("").concat(image.getExtension().name()));
-        return null;
+        // headers.setContentDispositionFormData("", image.getName().concat("").concat(image.getExtension().name()));
+        headers.setContentDispositionFormData("inline; filename \""
+                + image.getName()
+                + "\"" , image.getFileName());
+
+        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
+
     }
 
     //localhost:8080/v1/images/xxxxxxxxxxxxxx
